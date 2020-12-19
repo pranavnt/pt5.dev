b755cfd857f1f40fd7773a59c70ba4a525628e76
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-19 03:35:14 -0800

    project component

--- pages/components/project.js
+++ pages/components/project.js
@@ -3,8 +3,8 @@ import styles from "../styles/Home.module.css";
 const Project = (props) => {
   return (
     <a href={props.info.url} className={styles.card}>
-      <h3>Learn</h3>
-      <p>Learn about Next.js in an interactive course with quizzes!</p>
+      <h3>{props.info.name}</h3>
+      <p>{props.info.description}</p>
     </a>
   );
 };
