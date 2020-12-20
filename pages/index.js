393bc384e07dd1a99f70c81a17684525faa24a46
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-19 16:03:42 -0800

    added styling and some project information

--- pages/index.js
+++ pages/index.js
@@ -11,33 +11,60 @@ export default function Home() {
       </Head>
 
       <div style={{ float: "left" }}>
-        <h1 className={styles.title}>Hi! I'm Pranav Teegavarapu</h1>
+        <h1>Hi! I'm Pranav Teegavarapu</h1>
       </div>
 
-      <div className={styles.grid}>
-        <Project
-          url="https://nextjs.org/learn"
-          name="Learn"
-          description="Learn about Next.js in an interactive course with quizzes!"
-        />
+      <div style={{ float: "right" }}>
+        <h2>Projects</h2>
+        <div className={styles.grid}>
+          <Project
+            url="https://github.com/kobra-dev"
+            name="Kobra 🐍"
+            description="A visual programming language (like Scratch) for machine learning"
+          />
 
-        <Project
-          url="https://nextjs.org/learn"
-          name="Learn"
-          description="Learn about Next.js in an interactive course with quizzes!"
-        />
+          <Project
+            url="https://github.com/OtterLang/Otter"
+            name="Otter 🦦"
+            description="A programming language built for speed, readability, and ergonomics"
+          />
 
-        <Project
-          url="https://nextjs.org/learn"
-          name="Learn"
-          description="Learn about Next.js in an interactive course with quizzes!"
-        />
+          <Project
+            url="https://github.com/pranavnt/Walla"
+            name="Walla 🦘"
+            description="Walla is an intuitive, fast web framework for building APIs in Node"
+          />
 
-        <Project
-          url="https://nextjs.org/learn"
-          name="Learn"
-          description="Learn about Next.js in an interactive course with quizzes!"
-        />
+          <Project
+            url="https://github.com/pranavnt/pt5.dev"
+            name="pt5.dev 💻"
+            description="My personal website (What you're looking at right now)!"
+          />
+
+          <Project
+            url="https://devpost.com/software/Simplitize"
+            name="Simplitize 📖"
+            description="Using NLP-powered Question Answering to help users understand academic literature"
+          />
+
+          <Project
+            url="https://devpost.com/software/PhishingNet"
+            name="PhishingNet 🎣"
+            description="NLP-powered chrome extension to detect phishing websites"
+          />
+
+          <Project
+            url="https://github.com/pranavnt/Diffcheckr"
+            name="Diffcheckr ✅"
+            description="Minimal, intuitive diffchecker for comparing your output to test cases"
+          />
+
+          <Project
+            url="https://github.com/pranavnt"
+            name="GitHub 🔧"
+            description="Check out my GitHub to see any other projects I'm working on!"
+          />
+        </div>
       </div>
 
       <footer className={styles.footer}>
